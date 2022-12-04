4721481862115a78dec5b8c179aea028c5d0cadf
sunridden <kevinszeto9449@gmail.com>
2022-12-04 09:32:01 -0800

    Update javascript template

--- js/template.js
+++ js/template.js
@@ -1,6 +1,6 @@
 import { readFileSync } from "node:fs";
 
-const lines = readFileSync("day01.txt", { encoding: "utf-8" }) // read day??.txt content
+const lines = readFileSync("../../input/2022/day01.txt", { encoding: "utf-8" }) // read day??.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
   .trim() // Remove starting/ending whitespace
   .split("\n\n") // Split on newline
